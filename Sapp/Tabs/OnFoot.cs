037c4fd0426f1937d45d39cdd73dda4538043870
Tim Potze <timpotze@gmail.com>
2013-04-29 20:59:40 +0200

    Displaying this new data

--- Sapp/Tabs/OnFoot.cs
+++ Sapp/Tabs/OnFoot.cs
@@ -42,25 +42,32 @@ namespace Sapp.Tabs
 
             int money = GTA.gta.GetMemory(0xB7CE50).GetValue();
 
-            string time = GTA.gta.GetMemory(0xB70153).GetValue().ToString("00") + ":" + GTA.gta.GetMemory(0xB70152).GetValue().ToString("00");
+            string time = GTA.gta.GetMemory(0xB70153).GetByte().ToString("00") + ":" + GTA.gta.GetMemory(0xB70152).GetByte().ToString("00");
 
-            int weaponSlot = player.GetMemory(0x718).GetValue();
+            int weaponSlot = player.GetMemory(0x718).GetByte();
             
-            int weaponid = player.GetMemory(0x5A0 + 0x1C * weaponSlot + 0).GetValue();//MemoryReader.getVal(gta, PlayerPointer + 0x5A0 + 28 * weaponslot + 0);
-            int clip = player.GetMemory(0x5A0 + 0x1C * weaponSlot + 0x8).GetValue();
-            int remaining = player.GetMemory(0x5A0 + 0x1C * weaponSlot + 0xC).GetValue();
+            int weaponid = player.GetMemory(0x5A0 + 0x1C * weaponSlot + 0).GetShort();
+            int clip = player.GetMemory(0x5A0 + 0x1C * weaponSlot + 0x8).GetShort();
+            int remaining = player.GetMemory(0x5A0 + 0x1C * weaponSlot + 0xC).GetShort();
 
             //Drawing
             e.graphics.Clear(Color.White);
-            Bitmap weapon = (Bitmap)(Properties.Resources.ResourceManager.GetObject("weapon_0"));
-            e.graphics.DrawImage(weapon, new Point(0, 0));
+            Bitmap weapon = (Bitmap)(Properties.Resources.ResourceManager.GetObject("weapon_" + weaponid));
+            if(weapon != null)
+                e.graphics.DrawImage(weapon, new Point(0, 0));
 
-            e.graphics.DrawString("Armor:", Drawing.Fonts.Regular, Brushes.Black, new PointF(e.lcd.Width - 90, -1));
+            e.graphics.DrawString("Armor:", Drawing.Fonts.Regular, Brushes.Black, new Point(e.lcd.Width - 90, -1));
             Drawing.Progressbar.Draw(e.graphics, new Rectangle(e.lcd.Width - 52, 2, 50, 6), Direction.Right, (float)Math.Ceiling(armor / 100 * 100));
 
-            e.graphics.DrawString("Health:", Drawing.Fonts.Regular, Brushes.Black, new PointF(e.lcd.Width - 87, 7));
+            e.graphics.DrawString("Health:", Drawing.Fonts.Regular, Brushes.Black, new Point(e.lcd.Width - 87, 7));
             Drawing.Progressbar.Draw(e.graphics, new Rectangle(e.lcd.Width - 52, 10, 50, 6), Direction.Right, (float)Math.Ceiling(health / maxhealth * 100));
 
+            e.graphics.DrawString(locationName, Drawing.Fonts.Regular, Brushes.Black, new Point(1, 32));
+            e.graphics.DrawString("$" + money.ToString("00000000"), Drawing.Fonts.Big, Brushes.Black, new Point(e.lcd.Width - 68, 16));
+            e.graphics.DrawString(time, Drawing.Fonts.BigBold, Brushes.Black, new PointF(e.lcd.Width - 35, 28));
+
+            if (clip + remaining != 0)
+                e.graphics.DrawString((remaining - clip) + "-" + clip, Drawing.Fonts.Regular, Brushes.Black, new Point(34, 0));
 
         }
 
