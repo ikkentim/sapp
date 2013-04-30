88cfe219206f9101265236b1cd9c988316cd343e
Tim Potze <timpotze@gmail.com>
2013-04-30 16:37:13 +0200

    Completed InVehicle

---
+++ Sapp/Drawing/Car.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Sapp.Drawing
+{
+    static class Car
+    {
+        public static void DrawCar(Graphics g, int x, int y, int pop_lf, int pop_lr, int pop_rf, int pop_rr, int pop_show, int seat, bool[] seats)
+        {
+            Pen pen = new Pen(Brushes.Black);
+            if (pop_lf == 0 || (pop_lf == pop_show))
+                g.DrawRectangle(pen, new Rectangle(x + 0, y + 1, 1, 5));
+            if (pop_lr == 0 || (pop_lr == pop_show))
+                g.DrawRectangle(pen, new Rectangle(x + 0, y + 22, 1, 5));
+
+            if (pop_rf == 0 || (pop_rf == pop_show))
+                g.DrawRectangle(pen, new Rectangle(x + 19, y + 1, 1, 5));
+            if (pop_rr == 0 || (pop_rr == pop_show))
+                g.DrawRectangle(pen, new Rectangle(x + 19, y + 22, 1, 5));
+
+            if (seats.Length >= 1)
+            {
+                g.DrawRectangle(pen, new Rectangle(x + 4, y + 3, 5, 7));
+                if (seat == 0)
+                    g.FillRectangle(Brushes.Black, new Rectangle(x + 4, y + 3, 5, 7));
+                else if (seats[0])
+                    drawGrid(g, new Rectangle(x + 4, y + 3, 5, 7));
+            }
+
+            if (seats.Length >= 2)
+            {
+                g.DrawRectangle(pen, new Rectangle(x + 11, y + 3, 5, 7));
+                if (seat == 1)
+                    g.FillRectangle(Brushes.Black, new Rectangle(x + 11, y + 3, 5, 7));
+                else if (seats[1])
+                    drawGrid(g, new Rectangle(x + 11, y + 3, 5, 7));
+            }
+
+            if (seats.Length >= 3)
+            {
+                g.DrawRectangle(pen, new Rectangle(x + 4, y + 13, 5, 7));
+                if (seat == 2)
+                    g.FillRectangle(Brushes.Black, new Rectangle(x + 4, y + 13, 5, 7));
+                else if (seats[2])
+                    drawGrid(g, new Rectangle(x + 4, y + 13, 5, 7));
+            }
+
+            if (seats.Length >= 4)
+            {
+                g.DrawRectangle(pen, new Rectangle(x + 11, y + 13, 5, 7));
+                if (seat == 3)
+                    g.FillRectangle(Brushes.Black, new Rectangle(x + 11, y + 13, 5, 7));
+                else if (seats[3])
+                    drawGrid(g, new Rectangle(x + 11, y + 13, 5, 7));
+            }
+
+            g.DrawRectangle(pen, new Rectangle(x + 2, y + 0, 16, 28));
+        }
+
+        public static void DrawBike(Graphics g, int x, int y, int pop_f, int pop_r, int pop_show, int seat, bool[] seats)
+        {
+            Pen pen = new Pen(Brushes.Black);
+            if (pop_f == 0 || (pop_f == pop_show))
+                g.DrawRectangle(pen, new Rectangle(x + 2, y + 0, 1, 5));
+            if (pop_r == 0 || (pop_r == pop_show))
+                g.DrawRectangle(pen, new Rectangle(x + 2, y + 25, 1, 5));
+
+            if (seats.Length == 1)
+            {
+                g.DrawRectangle(pen, new Rectangle(x, y + 7, 5, 16));
+                if (seat == 0)
+                    g.FillRectangle(Brushes.Black, new Rectangle(x, y + 7, 5, 16));
+                else if (seats[0])
+                    drawGrid(g, new Rectangle(x, y + 7, 5, 16));
+            }
+            else
+            {
+                if (seats.Length > 1)
+                {
+                    g.DrawRectangle(pen, new Rectangle(x, y + 7, 5, 7));
+                    if (seat == 0)
+                        g.FillRectangle(Brushes.Black, new Rectangle(x, y + 7, 5, 7));
+                    else if (seats[0])
+                        drawGrid(g, new Rectangle(x, y + 7, 5, 7));
+                }
+
+                if (seats.Length >= 2)
+                {
+                    g.DrawRectangle(pen, new Rectangle(x, y + 16, 5, 7));
+                    if (seat == 1)
+                        g.FillRectangle(Brushes.Black, new Rectangle(x, y + 16, 5, 7));
+                    else if (seats[1])
+                        drawGrid(g, new Rectangle(x, y + 16, 5, 7));
+                }
+            }
+        }
+
+        private static void drawGrid(Graphics g, Rectangle rectangle)
+        {
+            Bitmap bm = new Bitmap(1, 1);
+            bm.SetPixel(0, 0, Color.Black);
+
+            for (int w = 0; w < rectangle.Width; w++)
+            {
+                for (int h = 0; h < rectangle.Height; h++)
+                {
+                    if ((w * rectangle.Width + h) % 2 == 1)
+                    {
+                        g.DrawImageUnscaled(bm, rectangle.X + w, rectangle.Y + h);
+                    }
+
+                }
+            }
+        }
+    }
+}
--- Sapp/Tabs/InVehicle.cs
+++ Sapp/Tabs/InVehicle.cs
@@ -5,12 +5,15 @@ using System.Text;
 using LogiFrame;
 using System.Drawing;
 using MemoryMaster.Memory;
+using System.Diagnostics;
 
 namespace Sapp.Tabs
 {
     class InVehicle : Tab
     {
+        bool flicker = false;
         LCDApplication app;
+
         public InVehicle(LCDApplication app)
         {
             this.app = app;
@@ -69,6 +72,10 @@ namespace Sapp.Tabs
             int nos = vehiclePointer.GetMemory(0x48A).GetByte();
             float nosStatus = vehiclePointer.GetMemory(0x8A4).GetFloat();
 
+            float speedx = vehiclePointer.GetMemory(68).GetFloat();
+            float speedy = vehiclePointer.GetMemory(72).GetFloat();
+            float speedz = vehiclePointer.GetMemory(76).GetFloat();
+            int speed = (int)Math.Round(Math.Sqrt(((speedx * speedx) + (speedy * speedy)) + (speedz * speedz)) * 136.666667);
 
             //Position memory
             Pointer vehicleMatrixPointer = vehiclePointer.GetPointer(0x14);
@@ -76,11 +83,6 @@ namespace Sapp.Tabs
             string locationName = SanAndreas.Data.GetLocationName(vehicleMatrixPointer.GetMemory(0x30).GetFloat(), vehicleMatrixPointer.GetMemory(0x34).GetFloat());
             float z = vehicleMatrixPointer.GetMemory(0x38).GetFloat();
 
-            float speedx = vehicleMatrixPointer.GetMemory(0x44).GetFloat();
-            float speedy = vehicleMatrixPointer.GetMemory(0x48).GetFloat();
-            float speedz = vehicleMatrixPointer.GetMemory(0x4C).GetFloat();
-
-            int speed = (int)Math.Round(Math.Sqrt(((speedx * speedx) + (speedy * speedy)) + (speedz * speedz)) * 136.666667)
 
             //Seats
             int seat = -1;
@@ -94,7 +96,67 @@ namespace Sapp.Tabs
                     seat = i;
             }
 
-            e.graphics.DrawString("veh.seat:" + seat, Drawing.Fonts.Regular, Brushes.Black, new Point(1, 1));
+            //Drawing 
+            e.graphics.DrawString(SanAndreas.Data.getVehicleName(model), Drawing.Fonts.Regular, Brushes.Black, new Point(0, 25));
+            e.graphics.DrawString(SanAndreas.Data.getRadioName(radioid), Drawing.Fonts.Regular, Brushes.Black, new Point(0, 0));
+            e.graphics.DrawString(locationName, Drawing.Fonts.Regular, Brushes.Black, new Point(0, 8));
+
+            //PopState
+            flicker = !flicker;
+            if (SanAndreas.Data.vehicleType(type) == SanAndreas.Vehicle.PLANECAR && !SanAndreas.Data.isPlane(model))
+            {
+                Drawing.Car.DrawCar(e.graphics, 135, 11, 
+                    vehiclePointer.GetMemory(0x5A5).GetByte(),
+                vehiclePointer.GetMemory(0x5A6).GetByte(),
+                vehiclePointer.GetMemory(0x5A7).GetByte(),
+                vehiclePointer.GetMemory(0x5A8).GetByte(),               
+                flicker?1:0, seat, steats);
+            }
+            else if (SanAndreas.Data.vehicleType(type) == SanAndreas.Vehicle.BIKE)
+            {
+                Drawing.Car.DrawBike(e.graphics, 145, 7,
+                    vehiclePointer.GetMemory(0x65C).GetByte(),
+                    vehiclePointer.GetMemory(0x65D).GetByte(),
+                    flicker?1:0, seat, steats);
+            }
+            else if (SanAndreas.Data.vehicleType(type) == SanAndreas.Vehicle.PLANECAR && SanAndreas.Data.isPlane(model))
+            {
+                Drawing.Progressbar.Draw(e.graphics, new Rectangle(145, 8, 8, 32), Direction.Up, (z % 250) / 5, true);
+            }
+            Drawing.Progressbar.Draw(e.graphics, new Rectangle(2, 35, 50, 6), Direction.Right, (float)Math.Ceiling(750 - (health - 250)) / 750 * 100);
+       
+            //Nitro
+            if (!(nosStatus > 0 && nos == 0))
+            {
+                if (nosStatus < 0.0)
+                    Drawing.Progressbar.Draw(e.graphics, new Rectangle(118, 8, 8, 32), Direction.Up, 100 - (-nosStatus * 100));
+                else
+                    Drawing.Progressbar.Draw(e.graphics, new Rectangle(118, 8, 8, 32), Direction.Down, (nosStatus * 100));
+                for (int i = 0; i < nos - ((nosStatus == 1 || nosStatus < 0) ? 1 : 0); i++)
+                {
+                    e.graphics.FillRectangle(Brushes.Black, new Rectangle(106 + ((i % 3) * 4), 31 + (((i - (i % 3)) / 3) * 4), 2, 2));
+                }
+            }
+
+            //Speed
+            StringFormat stringFormat = new StringFormat();
+            stringFormat.Alignment = StringAlignment.Near;
+            stringFormat.LineAlignment = StringAlignment.Near;
+            SizeF measure = e.graphics.MeasureString(speed.ToString("00"), Drawing.Fonts.BigBold);
+            e.graphics.DrawString(speed.ToString("00"), Drawing.Fonts.BigBold, Brushes.Black, new Point(108 - (int)measure.Width, e.lcd.Height - (int)measure.Height + 1));
+
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
